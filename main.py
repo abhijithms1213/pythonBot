3a5b475e3869c44b551cd5deea5d357290eb23f3
MidHunterX <midhunterx@gmail.com>
2026-03-03 11:13:11 +0530

    ✨ feat: greet user on hi

--- main.py
+++ main.py
@@ -15,24 +15,54 @@ logging.basicConfig(
 )
 
 
-async def vivi_greet(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def vivi_intro(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id if update.effective_chat else ""
+    if not chat_id:
+        return
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
+        chat_id=chat_id,
         text="Hello, I am Vivi!",
     )
 
 
-async def vivi_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def vivi_greet_member(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id if update.effective_chat else ""
+    if not chat_id:
+        return
+
+    user_message = update.message.text if update.message else ""
+    if not user_message:
+        return
+
+    username = update.effective_user.first_name if update.effective_user else ""
+    if not username:
+        return
+
+    vivi_reply = ""
+    if user_message.lower() == "hi":
+        vivi_reply = f"Hi, {username}!"
+
+    if not vivi_reply:
+        return
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=update.message.text,
+        chat_id=chat_id,
+        text=vivi_reply,
     )
 
 
 async def cmd_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if not context.args:
+        return
     text_caps = " ".join(context.args).upper()
+
+    chat_id = update.effective_chat.id if update.effective_chat else ""
+    if not chat_id:
+        return
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id,
+        chat_id=chat_id,
         text=text_caps,
     )
 
@@ -43,13 +73,13 @@ if __name__ == "__main__":
 
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    start_handler = CommandHandler("start", vivi_greet)
+    start_handler = CommandHandler("start", vivi_intro)
     application.add_handler(start_handler)
 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), vivi_reply)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), vivi_greet_member)
     application.add_handler(echo_handler)
 
-    caps_handler = CommandHandler('caps', cmd_caps)
+    caps_handler = CommandHandler("caps", cmd_caps)
     application.add_handler(caps_handler)
 
     application.run_polling()
