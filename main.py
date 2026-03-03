b01a96e348bbe864350246dfb65ab0e66ce89625
MidHunterX <midhunterx@gmail.com>
2026-03-03 09:16:40 +0530

    ✨ feat: test message reply handler

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 
 load_dotenv()
 
@@ -14,9 +14,17 @@ logging.basicConfig(
 )
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def vivi_greet(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, text="Test Message"
+        chat_id=update.effective_chat.id,
+        text="Hello, I am Vivi!",
+    )
+
+
+async def vivi_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=update.message.text,
     )
 
 
@@ -26,7 +34,10 @@ if __name__ == "__main__":
 
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    start_handler = CommandHandler("start", start)
+    start_handler = CommandHandler("start", vivi_greet)
     application.add_handler(start_handler)
+
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), vivi_reply)
+    application.add_handler(echo_handler)
 
     application.run_polling()
