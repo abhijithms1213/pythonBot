1869d3f189249676c9bcf41424bc59e536df31b3
abhijithms1213 <abhijithms1213@gmail.com>
2026-03-20 11:16:42 +0530

    fixed logic , now prints date or add new entries

--- db_management.py
+++ db_management.py
@@ -2,13 +2,36 @@ import sqlite3
 
 
 def addnewbatch(date, cursor):
-    sanitizedDate = f'{date}'.replace('-', '')
-    print(f'add new batch called {sanitizedDate}')
+    sanitizedDate = int(f'{date}'.replace('-', ''))
+    isfound = False
+    print(f'date is checking {sanitizedDate}')
     cursor.execute(f'''
-   select * from batches where Date_id = {sanitizedDate} 
+   select * from batches ;
                    ''')
+    # where Date_id = {sanitizedDate}
     output = cursor.fetchall()
-    print(output)
+    for row in output:
+        print(f'each row 1st attr: {row[0]}')
+        if row[0] == sanitizedDate:
+            cursor.execute(f'''
+        select Date_id from batches where Date_id = {sanitizedDate} 
+                             ''')
+            output = cursor.fetchone()
+            print(f'found in db {output}')
+            isfound = True
+        else:
+            continue
+
+    if not isfound:
+        plan_finish_date = sanitizedDate + 2
+        cursor.execute(f'''
+        insert into batches values ({sanitizedDate},{plan_finish_date},0,0);
+                    ''')
+        cursor.execute('select * from batches;')
+        output = cursor.fetchall()
+        print(output)
+    return None
+
 
 def dbops(operation, args):
     try:
@@ -22,5 +45,6 @@ def dbops(operation, args):
 
     finally:
         if connect:
+            connect.commit()
             connect.close()
             print('connection closed')
